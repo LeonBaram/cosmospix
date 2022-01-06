e0e99ec8f8d4626094d0dfb67381ded6703c13ad
LeonBaram <leonbaram@protonmail.com>
2022-01-06 02:05:58 -0500

    Implement basic image fetching demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { ApodData } from "./models";
+import { fetchImages, validateImages } from "./utils";
+
 export const App: React.FC = () => {
+  const [images, setImages] = useState<ApodData[]>();
+
+  useEffect(() => {
+    (async () => {
+      const apodData = await fetchImages();
+      setImages(apodData.filter(validateImages));
+      console.log(images);
+    })();
+  }, []);
   return (
     <>
       <h1>Cosmos Pix</h1>
