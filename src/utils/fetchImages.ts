dc72ef03511247b13d0bdc147fc7eb2f90df395f
LeonBaram <leonbaram@protonmail.com>
2022-01-06 02:51:25 -0500

    Add documentation to function fetchImages

--- src/utils/fetchImages.ts
+++ src/utils/fetchImages.ts
@@ -2,6 +2,11 @@ import { ApodParams, ApodData } from "../models";
 
 const apiBaseURL = "https://api.nasa.gov/planetary/apod";
 
+/**
+ * Sends a(n) HTTP GET request to the NASA APOD API, requesting the specified amount of ApodData instances, each of which may represent an image or a video with its associated metadata.
+ * @param count the number of ApodData instances for which to query the APOD API
+ * @returns [count] number of ApodData instances from the APOD API, held in a Promise
+ */
 export async function fetchImages(count: number): Promise<ApodData[]> {
   const params: ApodParams = {
     api_key: process.env.API_KEY,
