45b132a20fef4e4f471bf375842c2954701a9744
LeonBaram <leonbaram@protonmail.com>
2022-01-06 01:10:17 -0500

    Implement basic data fetching from NASA APOD API
    Create function fetchImages, which uses the built-in browser fetch
    function to send a HTTP GET request to NASA's APOD REST API. (Wow,
    that's a lot of acronyms. Sheesh.)

---
+++ src/utils/fetch-images.ts
@@ -0,0 +1,15 @@
+import { ApodResponseObject } from "../models";
+
+const apiBaseURL = "https://api.nasa.gov/planetary/apod";
+
+const params = new URLSearchParams({
+  api_key: process.env.API_KEY,
+  start_date: "2021-12-1",
+});
+
+const apiURL = `${apiBaseURL}?${params}`;
+
+export async function fetchImages(): Promise<ApodData> {
+  const response = await fetch(apiURL);
+  return response.json();
+}
